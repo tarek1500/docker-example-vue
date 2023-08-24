2c090ed0f6a533b316bf2f3e0a55ade4542630ed
Tarek Ibrahem <tarek_mohammed2@yahoo.com>
2023-08-24 17:50:37 +0300

    Added products view structure

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,5 +1,47 @@
-<template></template>
+<template>
+	<table>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Description</th>
+				<th>Created at</th>
+				<th>Updated at</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="product of products" :key="product.id">
+				<td>{{ product.id }}</td>
+				<td>{{ product.name }}</td>
+				<td>{{ product.description }}</td>
+				<td>{{ new Date(product.created_at).toISOString() }}</td>
+				<td>{{ new Date(product.updated_at).toISOString() }}</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { ref } from 'vue';
+	import Data from '../types/Data';
+	import Product from '../types/Product';
 
-<style></style>
+	let products = ref<Product[]>([]);
+
+	fetch('http://localhost:8000/api/products')
+		.then(response => response.json() as Promise<Data<Product>>)
+		.then(data => products.value = data.data)
+		.catch(error => console.log(error));
+</script>
+
+<style>
+	table {
+		margin: auto;
+	}
+
+	table,
+	th,
+	td {
+		border: 1px solid black;
+	}
+</style>
